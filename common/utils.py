132ceb368187116ffcb591e2935759264cc7fa60
AceKeppel <144319755+PlaxtonFlarion@users.noreply.github.com>
2025-05-22 10:09:37 +0800

    bug fixes

--- common/utils.py
+++ common/utils.py
@@ -13,19 +13,16 @@ from dotenv import load_dotenv
 fake = Faker()
 
 
-def current_env(url_name: str, key_name: str) -> tuple[str, str]:
+def current_env(*args, **__) -> dict[str, str]:
     if (env_path := Path(__file__).resolve().parents[1] / ".env").exists():
         load_dotenv(env_path)
-        service_url = os.getenv(url_name)
-        service_key = os.getenv(key_name)
-    else:
-        service_url = Path("/etc/secrets", key_name).read_text().strip()
-        service_key = Path("/etc/secrets", url_name).read_text().strip()
-
-    if not service_url or not service_key:
-        raise EnvironmentError("环境变量未正确加载，请检查 .env 或 Render 配置。")
-
-    return service_url, service_key
+        return {
+            arg: os.getenv(arg) for arg in args
+        }
+    
+    return {
+        arg: Path(f"/etc/secrets/{arg}").read_text().strip() for arg in args
+    }
 
 
 if __name__ == '__main__':
