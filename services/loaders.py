02001afdd5c41127ed03b6a048f2fd1ede52cf34
AceKeppel <AceKeppel@outlook.com>
2025-11-29 01:47:47 +0800

    bug fixes

--- services/loaders.py
+++ services/loaders.py
@@ -119,7 +119,7 @@ async def resolve_proxy_predict(
 
     license_info = {
         "configuration" : {},
-        "available"     : True,
+        "available"     : False,
         "expire_at"     : expire_at,
         "timeout"       : 60.0,
         "content_type"  : "multipart/form-data",
