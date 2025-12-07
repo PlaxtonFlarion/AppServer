3a831ed6791a0c7f5baf1281d3c379b23797a3ba
AceKeppel <AceKeppel@outlook.com>
2025-12-07 12:25:04 +0800

    bug fixes

--- services/domain/self_healing/candidate.py
+++ services/domain/self_healing/candidate.py
@@ -4,46 +4,33 @@
 # | |__| (_| | | | | (_| | | (_| | (_| | ||  __/
 #  \____\__,_|_| |_|\__,_|_|\__,_|\__,_|\__\___|
 #
-
+import asyncio
 import time
 import httpx
 import random
 import typing
 from loguru import logger
 from fastapi import Request
-from starlette.responses import JSONResponse
-
+from fastapi.responses import JSONResponse
 from schemas.cognitive import (
     HealRequest, HealResponse
 )
 from services.domain.standard import signature
-from services.domain.self_healing.parsing import AndroidXmlParser, WebDomParser
+from services.domain.self_healing.parsing import (
+    AndroidXmlParser, WebDomParser
+)
 from services.infrastructure.vector.zilliz import Zilliz
+from services.infrastructure.llm.llm_groq import LLMGroq
 from utils import const
 
 
-async def post_tensor(text_list: list) -> dict:
+async def delivery(url: str, json: dict, **kwargs) -> dict:
     expire_at = int(time.time()) + random.randint(3600, 86400)
     token     = signature.sign_token("Heal", expire_at)
     headers   = {const.TOKEN_FORMAT: token}
 
     async with httpx.AsyncClient(headers=headers, timeout=60) as client:
-        resp = await client.post(
-            const.MODAL_TENSOR, json={"texts": text_list}
-        )
-        resp.raise_for_status()
-        return resp.json()
-
-
-async def post_rerank(query: str, candidate: list) -> dict:
-    expire_at = int(time.time()) + random.randint(3600, 86400)
-    token     = signature.sign_token("Heal", expire_at)
-    headers   = {const.TOKEN_FORMAT: token}
-
-    async with httpx.AsyncClient(headers=headers, timeout=60) as client:
-        resp = await client.post(
-            const.MODAL_RERANK, json={"query": query, "candidate": candidate}
-        )
+        resp = await client.post(url, json=json, **kwargs)
         resp.raise_for_status()
         return resp.json()
 
@@ -53,8 +40,6 @@ async def heal_element(
     request: Request
 ) -> typing.Union[HealResponse, JSONResponse]:
 
-    store: Zilliz = request.app.state.store
-
     # 根据平台选择不同解析器，返回 ElementNode 列表
     logger.info(f"解析节点: {(app_platform := req.platform.strip().lower())}")
     match app_platform:
@@ -63,25 +48,36 @@ async def heal_element(
         # 视情况扩展 iOS / 其他
         case _: raise ValueError(f"Unsupported platform: {req.platform}")
 
+    query     = f"by={req.old_locator.by}, value={req.old_locator.value}"
     desc_list = [n.ensure_desc() for n in node_list]
 
-    logger.info(f"向量生成")
-    embedding_resp = await post_tensor(desc_list)
-    logger.info(f"[[v1], [v2], ...] 维度匹配")
-    page_vectors = embedding_resp["vectors"]
-
-    logger.info(f"插入向量")
-    for node, vec in zip(node_list, page_vectors):
-        store.insert(vec, node.ensure_desc())
-
-    query = f"by={req.old_locator.by}, value={req.old_locator.value}"
-    logger.info(f"构建文本: {query}")
+    logger.info(f"向量生成: {query}")
+    logger.info(f"向量生成: {len(desc_list)}")
     try:
-        query_vec = (await post_tensor([query]))["vectors"][0]
+        embedding_resp = await delivery(
+            const.MODAL_TENSOR, json={"query": query, "texts": desc_list}
+        )
     except Exception as e:
         return JSONResponse(content={"error": str(e)}, status_code=400)
 
-    logger.info(f"向量召回 {(k := 5)}")
+    store: Zilliz     = request.app.state.store
+    llm_groq: LLMGroq = request.app.state.llm_groq
+
+    logger.info(f"[[v1], [v2], ...] 维度匹配")
+    query_vec    = embedding_resp["query"]
+    page_vectors = embedding_resp["vectors"]
+
+    logger.info(f"插入向量")
+    await asyncio.gather(
+        *(asyncio.to_thread(store.insert, vec, node.ensure_desc())
+          for node, vec in zip(node_list, page_vectors))
+    )
+    # for node, vec in zip(node_list, page_vectors):
+    #     store.insert(vec, node.ensure_desc())
+
+    k, top_k, beta = 5, 3, 1 - (alpha := 0.1)
+
+    logger.info(f"向量召回 {k}")
     retrieved = store.search(query_vec, k=k)
 
     mapped_candidates: list[dict] = []
@@ -100,7 +96,7 @@ async def heal_element(
 
     logger.info(f"结果重排")
     try:
-        rerank_resp = await post_rerank(query, candidate)
+        rerank_resp = await delivery(const.MODAL_RERANK, json={"query": query, "candidate": candidate})
     except Exception as e:
         return JSONResponse(content={"error": str(e)}, status_code=400)
 
@@ -108,15 +104,14 @@ async def heal_element(
     for c, s in zip(mapped_candidates, rerank_scores):
         c["rerank_score"] = float(s)
 
-    logger.info(f"融合得分: 20% 用向量召回，80% 用 CrossEncoder")
-    alpha = 0.2
+    logger.info(f"融合模式: 向量({alpha * 100:.0f}%), CrossEncoder({beta * 100:.0f}%)")
     for c in mapped_candidates:
-        c["final_score"] = alpha * c["vector_score"] + (1 - alpha) * c["rerank_score"]
+        c["final_score"] = alpha * c["vector_score"] + beta * c["rerank_score"]
 
-    logger.info(f"取 top-3")
-    top_candidates = sorted(mapped_candidates, key=lambda x: x["final_score"], reverse=True)[:3]
+    logger.info(f"取 top-{top_k}")
+    top_candidates = sorted(mapped_candidates, key=lambda x: x["final_score"], reverse=True)[:top_k]
 
-    decision = await request.app.state.llm_groq.best_candidate(
+    decision = await llm_groq.best_candidate(
         req.old_locator, top_candidates
     )
     index  = decision["index"]
